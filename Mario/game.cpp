cccc2be6b6817f8e4e9f9429938ae1f29b829d1b
Георгий Киселёв <gosha_97@mail.ru>
2019-06-03 14:56:46 +0300

    Fixed

--- Mario/game.cpp
+++ Mario/game.cpp
@@ -9,6 +9,12 @@ float offsetY = 0;		//Sdvig po Y
 const int H = 16;
 const int W = 56;
 
+const float frameSpeed = 0.000007;
+
+const char platform = 'B';
+const char ground = 'G';
+const char boundaries = 'Z';
+const char bounty = '0';
 
 String TileMap[H] = {
 
@@ -31,6 +37,13 @@ String TileMap[H] = {
 
 }; //Karta v vide massiva
 
+enum Direction
+{
+	left = 0,
+	right,
+	stop
+};
+
 class Player {
 public:
 	float dx, dy, currentFrame;
@@ -42,14 +55,26 @@ public:
 		marioCoordinates = FloatRect(7 * 16, 12 * 16, 16, 32);
 
 		marioSprite.setTexture(image);
-		marioSprite.setTextureRect(IntRect(80, 1, 16, 32));
 
 		dx = 0;
 		dy = 0;
 		currentFrame = 0;
 	}
 
+	void animation(Direction direc, int currentFrame) {
+		const int stepToNextFrame = 17;
+
+		if (direc) {
+			marioSprite.setTextureRect(IntRect(80 + stepToNextFrame * currentFrame, 1, 16, 32));
+		}
+		else {
+			marioSprite.setTextureRect(IntRect(96 + stepToNextFrame * currentFrame, 1, -16, 32));
+		}
+	}
+
 	void move(float time) {
+		const float pachtiNull = 0.00002;
+
 		marioCoordinates.left += dx * time;
 		Interaction(0);
 
@@ -60,22 +85,20 @@ public:
 		onGround = false;
 		Interaction(1);
 
-
+		if ((dx > -pachtiNull) && (dx < pachtiNull)) {
+			animation(stop, 0);
+		}
 
 		if (dx > 0) {
-			marioSprite.setTextureRect(IntRect((80 + 17 * int(currentFrame)), 1, 16, 32));  //Animaciya
+			animation(right, int(currentFrame));
 		}
 
 		if (dx < 0) {
-			marioSprite.setTextureRect(IntRect((96 + 17 * int(currentFrame)), 1, -16, 32));
-		}
-
-		if ((dx > -0.00002) && (dx < 0.00002)) {
-			marioSprite.setTextureRect(IntRect(80, 1, 16, 32));
+			animation(left, int(currentFrame));
 		}
 
 
-		currentFrame += 0.000007*time;   //Skorost' animacii
+		currentFrame += frameSpeed * time;   //Skorost' animacii
 
 		if (currentFrame > 4)
 			currentFrame = 0;
@@ -86,10 +109,11 @@ public:
 	}
 
 	void Interaction(bool dir) {
+
 		for (int i = marioCoordinates.top / 16; i < (marioCoordinates.top + marioCoordinates.height) / 16; i++)			//Proverka tol'ko po koordinatam, kotorie zadevaet sprite Mario
 			for (int j = marioCoordinates.left / 16; j < (marioCoordinates.left + marioCoordinates.width) / 16; j++)
 			{
-				if ((TileMap[i][j] == 'B') || (TileMap[i][j] == 'G') || (TileMap[i][j] == 'Z'))
+				if ((TileMap[i][j] == platform) || (TileMap[i][j] == ground) || (TileMap[i][j] == boundaries))
 				{
 					if ((dx > 0) && (dir == 0))
 						marioCoordinates.left = j * 16 - marioCoordinates.width;
@@ -109,7 +133,7 @@ public:
 					}
 				}
 
-				if (TileMap[i][j] == '0')
+				if (TileMap[i][j] == bounty)
 				{
 					TileMap[i][j] = ' ';
 				}
@@ -158,7 +182,7 @@ int main() {
 
 		if (Keyboard::isKeyPressed(Keyboard::W)) {
 			if (mario.onGround) {
-				mario.dy = -0.00030;
+				mario.dy = -0.0003	 ;
 				mario.onGround = false;
 			}
 		}
@@ -177,27 +201,27 @@ int main() {
 		for (int i = 0; i < H; i++)					//Vivod spraitov
 			for (int j = 0; j < W; j++)
 			{
-				if (TileMap[i][j] == 'G') TileSprite.setTextureRect(IntRect(0, 0, 16, 16));
+				if (TileMap[i][j] == ground) TileSprite.setTextureRect(IntRect(0, 0, 16, 16));
 
-				if (TileMap[i][j] == 'B')
+				if (TileMap[i][j] == platform)
 					if (TileMap[i - 1][j] == ' ')
 					TileSprite.setTextureRect(IntRect(16, 0, 16, 16));
 					else
 					TileSprite.setTextureRect(IntRect(32, 0, 16, 16));
 
-				if (TileMap[i][j] == '0')  TileSprite.setTextureRect(IntRect(384 + 16 * int(currentFrame), 0, 16, 16));
+				if (TileMap[i][j] == bounty)  TileSprite.setTextureRect(IntRect(384 + 16 * int(currentFrame), 0, 16, 16));
 
-				if ((TileMap[i][j] == ' ') || (TileMap[i][j] == 'Z')) TileSprite.setTextureRect(IntRect(48, 336, 16, 16));
+				if ((TileMap[i][j] == ' ') || (TileMap[i][j] == boundaries)) TileSprite.setTextureRect(IntRect(48, 336, 16, 16));
 
 				if (TileMap[i][j] == '1') TileSprite.setTextureRect(IntRect(128, 320, 16, 16));
 				if (TileMap[i][j] == '2') TileSprite.setTextureRect(IntRect(144, 320, 16, 16));
 				if (TileMap[i][j] == '3') TileSprite.setTextureRect(IntRect(160, 320, 16, 16));
-
+				
 				TileSprite.setPosition(j * 16 - offsetX, i * 16 - offsetY);
 				window.draw(TileSprite);
 			}
 
-		currentFrame += 0.000007*time;
+		currentFrame += frameSpeed*time;
 
 		if (currentFrame > 3)
 			currentFrame = 0;
